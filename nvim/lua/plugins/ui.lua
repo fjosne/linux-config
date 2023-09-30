55af3d9614fbe1d928ce47a9f72350fa32a3e6d7
Simen Røe Fjøsne <simen-roe.fjosne@capgemini.com>
2023-09-30 19:15:43 +0200

    nvim: add dashboard plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -43,4 +43,14 @@ return {
             }
         end
     },
+    {
+        'glepnir/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup {
+                -- config
+            }
+        end,
+        dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    }
 }
